219e158acc90a2bc2aa69bad32a27e2bc3e6ed4a
Costa <marcelo.santos.costa@accenture.com>
2019-02-13 12:04:20 -0200

    primeiro commit - add base do projeto

---
+++ src/main/java/com/marcelo/controltimesheet/ControltimesheetApplication.java
@@ -0,0 +1,14 @@
+package com.marcelo.controltimesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControltimesheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControltimesheetApplication.class, args);
+	}
+
+}
+
